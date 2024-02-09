79ad5d17db98fea3afe19eb42bd5477f5c66d235
Alexander petree <apetree1001@email.phoenix.edu>
2024-02-09 04:50:45 -0800

    Update and rename pyproject.toml to project.php

---
+++ project.php
@@ -0,0 +1,15 @@
+
+tool.name "panoramix-decompiler"
+packages 
+include="panoramix,
+from="." version = beta
+description     
+A  decompiler     
+forthe EVM.
+authors  alexanderpetree 
+apetree1001@email.phoenix.edu 
+version  beta  
+allow-prereleases 
+true Time 0445  pst 
+tool panoramix build system
+build-backend core.api"
